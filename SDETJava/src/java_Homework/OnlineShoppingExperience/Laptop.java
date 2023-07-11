7dd7dfc0b9250f6323a3a516a821e31aaae86a74
Joshi, Varun <Varun.Joshi@mmc.com>
2023-07-11 12:19:23 +0530

    Homework - Online Shopping experience

---
+++ SDETJava/src/java_Homework/OnlineShoppingExperience/Laptop.java
@@ -0,0 +1,38 @@
+package java_Homework.OnlineShoppingExperience;
+
+public class Laptop extends Product implements Purchasable {
+	//Class variable
+	private double memory;
+	//constructor to initialise variables
+	public Laptop(String laptopName, double price, double memory) {
+
+		this.productType = "Laptop";
+		this.productName = laptopName;
+		this.price = price;
+		this.memory = memory;
+
+	}
+
+	@Override
+	public double calculatePrice() {
+		// TODO Auto-generated method stub
+		return price;
+	}
+
+	@Override
+	public void displayProductDetails() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Laptop Details:");
+		System.out.println("Name: " + productName);
+		System.out.println("Memory(GB): " + memory);
+
+	}
+
+	@Override
+	double getPrice() {
+		// TODO Auto-generated method stub
+		return price;
+	}
+
+}
