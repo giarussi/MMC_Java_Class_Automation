ef3e3250cac5afbec53c08c5b9a747c9087d8fd6
Bhat <Rakesh.Bhat@mmc.com>
2023-07-21 19:13:50 +0530

    rakesh bhat calc code

---
+++ payCalcOOPs/RegularEmployee.java
@@ -0,0 +1,24 @@
+package payCalcOOPs;
+
+import java.util.Scanner;
+
+public class RegularEmployee extends Employee {
+    private int maxHoursWorked = 40;
+    private double minHoursWorked = 0.25;
+
+    public RegularEmployee() {
+        super("regular");
+    }
+
+    public double calculateSalary(double... args) {
+    	double regularEmpHourlyPayRate = args[0];
+        double hoursWorked = args[1];
+
+        while (hoursWorked > maxHoursWorked || hoursWorked < minHoursWorked) {
+            System.out.println("Invalid employee hours.Please enter valid hours for this week (0.25-40 hours): ");
+            hoursWorked = new Scanner(System.in).nextDouble();
+        }
+
+        return regularEmpHourlyPayRate * hoursWorked;
+    }
+}
---
+++ payCalcOOPs/payCalc.java
@@ -0,0 +1,9 @@
+package payCalcOOPs;
+
+public class payCalc {
+    public static void main(String[] args) {
+    	PayCalcMethods payCalculator = new PayCalcMethods();
+        payCalculator.runPayCalculator();
+    }
+}
+
