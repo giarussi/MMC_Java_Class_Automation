1e2ffb088b9a3542dffebce63ea2f95139a22a09
Bhat <Rakesh.Bhat@mmc.com>
2023-07-25 12:37:11 +0530

    exception handling by Rakesh

---
+++ exceptionHandling/WrongArgumentException.java
@@ -0,0 +1,23 @@
+package exceptionHandling;
+
+
+public class WrongArgumentException extends Exception {
+
+	private String exceptionMessage;
+	
+	
+	public WrongArgumentException() {
+		this.exceptionMessage = "Exception occured!! The value provided is not an Integer.";
+	}
+	
+	
+	public WrongArgumentException(String message) {
+		this.exceptionMessage = message;
+	}
+	
+	@Override
+	public String getMessage() {
+		return exceptionMessage;
+	}
+
+}
