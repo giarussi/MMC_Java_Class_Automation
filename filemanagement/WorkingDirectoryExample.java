1bd3a79f638f386828cd9a232a31cb5b4c44506b
shwetadf <36842587+shwetadf@users.noreply.github.com>
2023-07-28 11:51:37 +0530

    Delete WorkingDirectoryExample.java

--- filemanagement/WorkingDirectoryExample.java
+++
@@ -1,8 +0,0 @@
-package filemanagement;
-
-public class WorkingDirectoryExample {
-    public static void main(String[] args) {
-        String workingDir = System.getProperty("user.dir");
-        System.out.println("Working Directory: " + workingDir);
-    }
-}
