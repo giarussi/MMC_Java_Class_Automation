48034ea8560d74dda1d225a11a78a8a5bd54ffa7
Siddhant <siddhant.pednekar@mmc.com>
2023-08-10 13:18:24 +0530

    Calculator Exception Handling
    Calculator Exception Handling Assignment Submitted.

---
+++ Calculator Exception Handling/DivisionByZeroException.java
@@ -0,0 +1,37 @@
+package java_Homework.ExceptionHandling;
+
+/**
+ * Exception class which occurs when division by 0 is attempted
+ * 
+ * @author U1234713
+ *
+ */
+
+public class DivisionByZeroException extends Exception {
+
+	private String exceptionMessage;
+
+	/**
+	 * Constructor thats sets default exception message
+	 */
+	public DivisionByZeroException() {
+		this.exceptionMessage = "Exception occured!! Number cannot be divided by 0";
+	}
+
+	/**
+	 * Constructor that accepts exception message from user
+	 * 
+	 * @param message
+	 */
+	public DivisionByZeroException(String message) {
+		this.exceptionMessage = message;
+	}
+
+	/**
+	 * Returns exception message
+	 */
+	@Override
+	public String getMessage() {
+		return exceptionMessage;
+	}
+}
---
+++ Calculator Exception Handling/WrongArgumentException.java
@@ -0,0 +1,36 @@
+package java_Homework.ExceptionHandling;
+
+/**
+ * Exception class which occurs when input is not an integer
+ * 
+ * @author U1234713
+ *
+ */
+public class WrongArgumentException extends Exception {
+
+	private String exceptionMessage;
+	
+	/**
+	 * Constructor thats sets default exception message
+	 */
+	public WrongArgumentException() {
+		this.exceptionMessage = "Exception occured!! The value provided is not an Integer.";
+	}
+	
+	/**
+	 * Constructor that accepts exception message from user
+	 * @param message
+	 */
+	public WrongArgumentException(String message) {
+		this.exceptionMessage = message;
+	}
+	
+	/**
+	 * Returns exception message
+	 */
+	@Override
+	public String getMessage() {
+		return exceptionMessage;
+	}
+
+}
